61f25cfc8ea143220165ae413349aa6905c96603
vighanesh295 <vighaneshtarade295@gmail.com>
2026-07-02 15:35:59 +0530

    Update Android build configuration

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -16,8 +16,23 @@ subprojects {
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
 subprojects {
+    afterEvaluate {
+        if (project.hasProperty("android")) {
+            val androidExt = project.extensions.getByName("android")
+            try {
+                androidExt.javaClass.getMethod("setCompileSdkVersion", Int::class.java).invoke(androidExt, 36)
+            } catch (e: Exception) {
+                // Ignore if method not found
+            }
+        }
+    }
+}
+
+subprojects {
     project.evaluationDependsOn(":app")
 }
+
+
 
 
 tasks.register<Delete>("clean") {
